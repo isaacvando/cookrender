d1b008ad411528fa0ffca30c6b5c5569249c5cf1
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-02 15:47:01 -0600

    refactored main method

--- src/extension.ts
+++ src/extension.ts
@@ -3,22 +3,21 @@ import { TextEncoder } from 'util';
 import { Parser, ParseResult } from '@cooklang/cooklang-ts';
 
 export function activate(context: vscode.ExtensionContext) {
+	// const watcher = vscode.workspace.createFileSystemWatcher("*.cook", true, false, true);
 	let disposable = vscode.commands.registerCommand('cookrender.render', () => {
 		const editor = vscode.window.activeTextEditor;
+		if (!editor)
+			return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
 
-		if(editor) {
-			const uri: vscode.Uri = editor.document.uri;
-			const fileName: string | undefined = uri.toString().split('/').pop();
+		const uri: vscode.Uri = editor.document.uri;
+		const fileName: string | undefined = uri.toString().split('/').pop();
 
-			if (fileName?.split('.').length === 2 && fileName?.split('.').pop() === "cook") {
-				const fileNameNoExt: string = fileName.split('.')[0];
-				const md: string = getMarkdown(new Parser().parse(editor.document.getText()), fileNameNoExt);
-				const targetUri: string = uri.toString().slice(0, -fileName.length) + fileNameNoExt + '.md';
-				vscode.workspace.fs.writeFile(vscode.Uri.parse(targetUri), new TextEncoder().encode(md));
-				vscode.window.showInformationMessage(`Created ${fileNameNoExt}.md`);
-			} else {
-				vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
-			}
+		if (fileName?.split('.').length === 2 && fileName?.split('.').pop() === "cook") {
+			const fileNameNoExt: string = fileName.split('.')[0];
+			const md: string = getMarkdown(new Parser().parse(editor.document.getText()), fileNameNoExt);
+			const targetUri: string = uri.toString().slice(0, -fileName.length) + fileNameNoExt + '.md';
+			vscode.workspace.fs.writeFile(vscode.Uri.parse(targetUri), new TextEncoder().encode(md));
+			vscode.window.showInformationMessage(`Created ${fileNameNoExt}.md`);
 		} else {
 			vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
 		}
@@ -51,12 +50,7 @@ function getMarkdown(parsed: ParseResult, fileName: string): string {
 	parsed.steps.forEach((step) => {
 		md += "- ";
 		step.forEach((part) => {
-			if(part.type === "text") {
-				md += escapeMD(part.value);
-			}
-			else {
-				md += `**${escapeMD(part.name).trim()}**`;
-			}
+			md += part.type === "text" ? escapeMD(part.value) : `**${escapeMD(part.name).trim()}**`;
 		});
 		md += "\n";
 	});
