02eedf024b55201d6ef2ccb424904ac324a73770
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-07 17:05:09 -0600

    refactored to use posix

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { TextEncoder } from 'util';
 import { Parser, ParseResult } from '@cooklang/cooklang-ts';
+import { posix } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('cookrender.enableRendering', () => {
@@ -21,21 +22,19 @@ export function deactivate() {}
 
 function render() {
 	const editor = vscode.window.activeTextEditor;
-		if (!editor)
-			return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
+	if (!editor)
+		return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
 
-		const uri: vscode.Uri = editor.document.uri;
-		const fileName: string | undefined = uri.toString().split('/').pop();
+	const uri: vscode.Uri = editor.document.uri;
+	const fileName = posix.basename(editor.document.fileName);
+	if (posix.extname(fileName) !== ".cook")
+		return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
 
-		if (fileName?.split('.').length === 2 && fileName?.split('.').pop() === "cook") {
-			const fileNameNoExt: string = fileName.split('.')[0];
-			const md: string = getMarkdown(new Parser().parse(editor.document.getText()), fileNameNoExt);
-			const targetUri: string = uri.toString().slice(0, -fileName.length) + fileNameNoExt + '.md';
-			vscode.workspace.fs.writeFile(vscode.Uri.parse(targetUri), new TextEncoder().encode(md));
-			vscode.window.showInformationMessage(`Wrote to ${fileNameNoExt}.md`);
-		} else {
-			vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
-		}
+	const fileNameNoExt: string = fileName.split('.')[0];
+	const md: string = getMarkdown(new Parser().parse(editor.document.getText()), fileNameNoExt);
+	const targetUri: string = uri.toString().slice(0, -fileName.length) + fileNameNoExt + '.md';
+	vscode.workspace.fs.writeFile(vscode.Uri.parse(targetUri), new TextEncoder().encode(md));
+	vscode.window.showInformationMessage(`Wrote to ${fileNameNoExt}.md`);
 }
 
 function getMarkdown(parsed: ParseResult, fileName: string): string {
