8d034bd68a6fa11e654c0369e48b513b4490db67
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2022-12-07 21:27:29 -0600

    changed to constantly updated, changed messages

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,10 @@ import { posix } from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('cookrender.enableRendering', () => {
-		vscode.window.showInformationMessage("Render on save enabled");
+		vscode.window.showInformationMessage("Rendering enabled");
 		render();
 		vscode.workspace.onDidChangeTextDocument((event) => {
-			// update .md file every time the .cook file is saved
-			if (!event.document.isDirty) {
-				render();
-			}
+			render();
 		});
 	});
 
@@ -23,18 +20,18 @@ export function deactivate() {}
 function render() {
 	const editor = vscode.window.activeTextEditor;
 	if (!editor)
-		return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
+		return;
 
 	const uri: vscode.Uri = editor.document.uri;
 	const fileName = posix.basename(editor.document.fileName);
 	if (posix.extname(fileName) !== ".cook")
-		return vscode.window.showInformationMessage('Unable to render; Please open a .cook file');
+		return;
 
 	const fileNameNoExt: string = fileName.split('.')[0];
 	const md: string = getMarkdown(new Parser().parse(editor.document.getText()), fileNameNoExt);
 	const targetUri: string = uri.toString().slice(0, -fileName.length) + fileNameNoExt + '.md';
 	vscode.workspace.fs.writeFile(vscode.Uri.parse(targetUri), new TextEncoder().encode(md));
-	vscode.window.showInformationMessage(`Wrote to ${fileNameNoExt}.md`);
+	// vscode.window.showInformationMessage(`Wrote to ${fileNameNoExt}.md`);
 }
 
 function getMarkdown(parsed: ParseResult, fileName: string): string {
@@ -49,12 +46,12 @@ function getMarkdown(parsed: ParseResult, fileName: string): string {
 		md += `- **${amount}** ${ingredient.name}\n`;
 	});
 
-	md += parsed.cookwares.length ? "\n## Cookware\n" : "";
+	md += parsed.cookwares.length ? "## Cookware\n" : "";
 	parsed.cookwares.forEach((cookware) => {
 		md += `- **${cookware.quantity}** ${cookware.name}\n`;
 	});
 
-	md += parsed.steps.length ? "\n## Steps\n" : "";
+	md += parsed.steps.length ? "## Steps\n" : "";
 	parsed.steps.forEach((step) => {
 		md += "- ";
 		step.forEach((part) => {
